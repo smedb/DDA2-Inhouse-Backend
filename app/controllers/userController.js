2c68534020c9e0df643746164811715c500a8e60
andrea.celtich <andrea.celtich@kavak.com>
2024-06-17 21:10:00 -0300

    fix: coverage

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -15,7 +15,7 @@ const {
     AWS_CREATE_USER_EMPLOYEE_SQS_MESSAGE,
     AWS_DELETE_USER_EMPLOYEE_SQS_MESSAGE
 } = require('../helpers/constants');
-const { userBuilderOrchestrator } = require('../helpers/userHelper');
+const { userBuilderOrchestrator, userInfoUpdate } = require('../helpers/userHelper');
 const { sendSNSEvent } = require('../services/awsSQS');
 
 const create = async (req, res, next) =>
@@ -24,26 +24,7 @@ const create = async (req, res, next) =>
         .catch(error => res.status(500).send({message: error.message}));
 
 cron.schedule('*/1 * * * *', async () => {
-    await userSchema.find({ 
-        approved: APPROVED_STATUS_PENDING,
-        fraudSituation: { $exists: false },
-        segment: USER_SEGMENT_CLIENT
-    })
-    .then(data => Promise.all(data.map(async currentUser => {
-        const completeUser = await userBuilderOrchestrator(JSON.parse(JSON.stringify(currentUser)));
-            return await userSchema.findOneAndUpdate({ _id: currentUser._id}, { 
-                    ...completeUser
-            }, { new: true })
-            .then(usr => usr)
-            .catch(error =>logger.info(error))
-        }))
-    )
-    .then(async users => {
-        return await Promise.all(
-        users.filter(usr=> usr.approved != APPROVED_STATUS_PENDING)
-            .map(approvedUsr => sendSNSEvent({email: approvedUsr.email, status: approvedUsr.approved}, AWS_CREATE_USER_CLIENT_SQS_MESSAGE))
-        )}).catch(error =>logger.info(error))
-    ;
+    await userInfoUpdate();
     logger.info('Cron running every one minute');
   });
 
--- app/helpers/userHelper.js
+++ app/helpers/userHelper.js
@@ -3,10 +3,16 @@ const {
     APPROVED_STATUS_REJECTED,
     CREDIT_SCORE_VALIDATION_TRUSTWORTHY,
     APPROVED_STATUS_APPROVED,
-    BIOMETRIC_VALIDATION_VERIFIED
+    BIOMETRIC_VALIDATION_VERIFIED,
+    APPROVED_STATUS_PENDING,
+    USER_SEGMENT_CLIENT,
+    AWS_CREATE_USER_CLIENT_SQS_MESSAGE
 } = require('../helpers/constants');
+const logger = require('../../logger');
+const userSchema = require('../models/user');
 const { predictCreditScore } = require('../helpers/creditScoreHelper');
 const { parseBiometricStatus } = require('../helpers/rekognitionHelper');
+const { sendSNSEvent } = require('../services/awsSQS');
 
 const userBuilderOrchestrator = async (userData) => {
     const user = {
@@ -23,6 +29,28 @@ const userBuilderOrchestrator = async (userData) => {
     return user;
 }
 
+const userInfoUpdate = async () =>
+    await userSchema.find({ 
+        approved: APPROVED_STATUS_PENDING,
+        fraudSituation: { $exists: false },
+        segment: USER_SEGMENT_CLIENT
+    })
+    .then(data => Promise.all(data.map(async currentUser => {
+        const completeUser = await userBuilderOrchestrator(JSON.parse(JSON.stringify(currentUser)));
+            return await userSchema.findOneAndUpdate({ _id: currentUser._id}, { 
+                    ...completeUser
+            }, { new: true })
+            .then(usr => usr)
+            .catch(error =>logger.info(error))
+        }))
+    )
+    .then(async users => {
+        return await Promise.all(
+        users.filter(usr=> usr.approved != APPROVED_STATUS_PENDING)
+            .map(approvedUsr => sendSNSEvent({email: approvedUsr.email, status: approvedUsr.approved}, AWS_CREATE_USER_CLIENT_SQS_MESSAGE))
+        )}).catch(error =>logger.info(error));
+
 module.exports = {
-    userBuilderOrchestrator
+    userBuilderOrchestrator,
+    userInfoUpdate
 }
